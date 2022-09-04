f3f6c6a53456ae1d3563fdd2abe65c8fe86cd5d1
Hyeonjun Park <phjppo0918@gmail.com>
2022-09-04 21:54:47 +0900

    카드2

---
+++ src/queue/bj2164/Main.java
@@ -0,0 +1,38 @@
+package queue.bj2164;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder result = new StringBuilder();
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        int cardCount = Integer.parseInt(st.nextToken());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= cardCount; i++) {
+            queue.add(i);
+        }
+        while (queue.size() > 1) {
+            queue.poll();
+            int temp = queue.poll();
+            queue.add(temp);
+        }
+
+        result.append(queue.poll());
+        
+        bw.write(result.toString());
+    
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
