c6c1b0e2acdbc545e063d16226bcf284f3d0b5de
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-22 22:29:53 +0900

    1로 만들기

---
+++ src/dp/bj1463/Main.java
@@ -0,0 +1,33 @@
+package dp.bj1463;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder result = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int value = Integer.parseInt(st.nextToken());
+        int dp[] = new int[value + 1];
+        for (int i = 2; i < value + 1; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+        }
+        result.append(dp[value]);
+
+
+        bw.write(result.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
